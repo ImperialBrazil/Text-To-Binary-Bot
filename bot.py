f94c154a0e0a622af0fd3a43e1aa507d6688418b
Fierce coder <74656394+coder8jedi@users.noreply.github.com>
2021-08-04 19:24:12 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,33 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+from app.config_reader import load_config
+from app.handlers.binary_to_text import register_binary_to_text
+from app.handlers.text_to_binary import register_text_to_binary
+
+logger = logging.getLogger(__name__)
+
+
+async def main():
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+    )
+    logger.error("Starting bot")
+    #
+    config = load_config("config/bot.ini")
+    #
+    bot = Bot(token=config.bot.TOKEN)
+    dp = Dispatcher(bot, storage=MemoryStorage())
+    #
+    register_binary_to_text(dp)
+    register_text_to_binary(dp)
+    #
+    await dp.start_polling()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
