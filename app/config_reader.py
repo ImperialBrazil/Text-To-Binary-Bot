19520d298dc64584c3666210015534e05d7751f7
Fierce coder <74656394+coder8jedi@users.noreply.github.com>
2021-08-04 19:27:08 +0300

    Add files via upload

---
+++ app/config_reader.py
@@ -0,0 +1,25 @@
+import configparser
+from dataclasses import dataclass
+
+
+@dataclass
+class Bot:
+    TOKEN: str
+
+
+@dataclass
+class Config:
+    bot: Bot
+
+
+def load_config(path: str):
+    config = configparser.ConfigParser()
+    config.read(path)
+
+    bot = config["bot"]
+
+    return Config(
+        bot=Bot(
+            TOKEN=bot["TOKEN"],
+        )
+    )
